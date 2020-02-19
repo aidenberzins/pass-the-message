7379ccfcb895ef0bc869d5398bc3a59969fe96be
Aiden Berzins <aiden.berzins@hotmail.com>
2020-02-18 19:08:49 -0700

    added code to append span to prvious message list

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+const msg = document.querySelector(".lastMessage");
+
+button.addEventListener("click", function() {
+  event.preventDefault();
+  let text = document.querySelector("#message").value;
+  let message = document.createElement("span");
+  message.innerHTML = text;
+  msg.append(message);
+  console.log(message);
+});
